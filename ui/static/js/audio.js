c6c3399418cab8a87c30cf4f37cac09618d202d0
Hoang Huong | apikey:d0851193d83d8c0dc0833e050f2c432f59dd585145eb42c2440363789246f383 <huong.ph@fsofts.com>
2026-09-01 11:59:09 +0700

    save

--- ui/static/js/audio.js
+++ ui/static/js/audio.js
@@ -2,6 +2,10 @@ function playMomoSound() {
     try {
         const audio = new Audio('/static/nhac_chuong_pokemon_black_and_white_tiktok-www_tiengdong_com.mp3');
         audio.play();
+        setTimeout(() => {
+            audio.pause();
+            audio.currentTime = 0;
+        }, 15000);
     } catch(e) {
         console.log("Audio not supported");
     }
